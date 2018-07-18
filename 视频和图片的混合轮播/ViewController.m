365b7f552305387fbc9514c8cb923e8e30c6cc19
会骑牛的小七 <wangshuai@huiqiniudexiaoqi-2.local>
2018-07-18 11:28:02 +0800

    更换视频地址

--- 视频和图片的混合轮播/ViewController.m
+++ 视频和图片的混合轮播/ViewController.m
@@ -26,7 +26,7 @@
     /**
      测试  数组首位为视频播放的地址，其余为本地图片，可根据实际需要进行更改
      **/
-    XQCarousel *carousel = [XQCarousel scrollViewFrame:CGRectMake(0, 64, self.view.frame.size.width, 300) imageStringGroup:@[@"http://yun.it7090.com/video/XHLaunchAd/video03.mp4", @"1.jpg", @"2.jpg", @"3.jpg"]];
+    XQCarousel *carousel = [XQCarousel scrollViewFrame:CGRectMake(0, 64, self.view.frame.size.width, 300) imageStringGroup:@[@"http://221.228.226.5/15/t/s/h/v/tshvhsxwkbjlipfohhamjkraxuknsc/sh.yinyuetai.com/88DC015DB03C829C2126EEBBB5A887CB.mp4", @"1.jpg", @"2.jpg", @"3.jpg"]];
     [self.view addSubview:carousel];
     
 }
